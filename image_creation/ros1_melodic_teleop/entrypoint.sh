378d766e725c8905645662952f21d7d978fd4c47
Jürgen Kottise <jyrgenkottise@gmail.com>
2025-05-20 17:45:49 +0300

    changed names

--- image_creation/ros1_melodic_teleop/entrypoint.sh
+++ image_creation/ros1_melodic_teleop/entrypoint.sh
@@ -5,8 +5,9 @@ service ssh start
 
 #source ROS setup files
 source /opt/ros/$ROS_DISTRO/setup.bash
-source /root/ros2_ws/install/local_setup.bash
+source /root/ros_ws/devel/setup.bash
 
+#rosrun teleop_twist_keyboard teleop_twist_keyboard.py
 
 #execute the provided command
 exec "$@"
--- image_creation/ros2_jazzy_teleop/entrypoint.sh
+++ image_creation/ros2_jazzy_teleop/entrypoint.sh
@@ -7,6 +7,7 @@ service ssh start
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /root/ros2_ws/install/local_setup.bash
 
+#ros2 launch demo_teleop teleop_joy.launch
 
 #execute the provided command
 exec "$@"
