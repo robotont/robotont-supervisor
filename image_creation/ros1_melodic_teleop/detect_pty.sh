f6293e6371bd6e7d618d165846692edc5834e28e
Jürgen Kottise <130277880+jyrgx@users.noreply.github.com>
2025-06-08 21:55:48 +0300

    align service name

--- image_creation/ros1_melodic_teleop/detect_pty.sh
+++ image_creation/ros1_melodic_teleop/detect_pty.sh
@@ -16,7 +16,7 @@ OVERRIDE_FILE=${OVERRIDE_FILE:-docker-compose.override.yml}
 #update the docker-compose.override.yml with the detected PTY
 cat > "$OVERRIDE_FILE" <<EOF
 services:
-  ros2_robotont_driver:
+  ros1_melodic_teleop:
     volumes:
       - "${PTY}:/dev/ttyACM0"
 EOF
